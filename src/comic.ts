e1cbb57afee404c4b0b8089ab6ff0df79ebd5420
Azamat Bayramov <bayramov.azamat04@gmail.com>
2024-06-28 10:11:35 +0300

    Improve code

--- src/comic.ts
+++ src/comic.ts
@@ -2,12 +2,12 @@ import { formatDistance } from "date-fns";
 
 const email = "a.bayramov@innopolis.university";
 
-const fetchJokeIdUrl = "https://fwd.innopolis.university/api/hw2?";
-const fetchJokeUrl = "https://fwd.innopolis.university/api/comic?";
+const fetchJokeIdUrl: string = "https://fwd.innopolis.university/api/hw2?";
+const fetchJokeUrl: string = "https://fwd.innopolis.university/api/comic?";
 
-const jokeTitle = document.getElementById("jokeTitle") as HTMLElement;
-const jokeDate = document.getElementById("jokeDate") as HTMLElement;
-const jokeImage = document.getElementById("jokeImage") as HTMLImageElement;
+const jokeTitle: HTMLElement = document.getElementById("jokeTitle") as HTMLElement;
+const jokeDate: HTMLElement = document.getElementById("jokeDate") as HTMLElement;
+const jokeImage: HTMLImageElement = document.getElementById("jokeImage") as HTMLImageElement;
 
 
 interface JokeDTO {
@@ -26,8 +26,8 @@ interface JokeDTO {
 
 
 async function fetchJokeId(email: string): Promise<string> {
-    const params = new URLSearchParams({"email": email});
-    const response = await fetch(fetchJokeIdUrl + params.toString());
+    const params = new URLSearchParams({"email": email}) as URLSearchParams;
+    const response = await fetch(fetchJokeIdUrl + params.toString()) as Response;
 
     if (!response.ok) {
         throw new Error("Failed to fetch joke id: ${response.statusText}");
@@ -35,13 +35,13 @@ async function fetchJokeId(email: string): Promise<string> {
 
     const jokeId: string = await response.json() as string;
 
-    return jokeId;
+    return jokeId as string;
 }
 
 
 async function fetchJoke(id: string): Promise<JokeDTO> {
-    const params = new URLSearchParams({"id": id});
-    const response = await fetch(fetchJokeUrl + params.toString());
+    const params = new URLSearchParams({"id": id}) as URLSearchParams;
+    const response = await fetch(fetchJokeUrl + params.toString()) as Response;
 
     if (!response.ok) {
         throw new Error("Failed to fetch joke: ${response.statusText}");
@@ -49,26 +49,25 @@ async function fetchJoke(id: string): Promise<JokeDTO> {
 
     const joke: JokeDTO = await response.json() as JokeDTO;
 
-    return joke;
+    return joke as JokeDTO;
 }
 
 
 function showJoke(joke: JokeDTO): void {
-    jokeTitle.innerText = joke.safe_title;
+    jokeTitle.innerText = joke.safe_title as string;
 
-    const date = new Date(parseInt(joke.year), parseInt(joke.month) - 1, parseInt(joke.day));
-    jokeDate.innerText = formatDistance(date, new Date(), { addSuffix: true })
+    const date: Date = new Date(parseInt(joke.year), parseInt(joke.month) - 1, parseInt(joke.day)) as Date;
+    jokeDate.innerText = formatDistance(date, new Date(), { addSuffix: true }) as string;
 
-    jokeImage.src = joke.img;
-    jokeImage.alt = joke.transcript;
+    jokeImage.src = joke.img as string;
+    jokeImage.alt = joke.transcript as string;
 }
 
 
-async function main() {
+async function main(): Promise<void> {
     try {
-        const jokeId = await fetchJokeId(email);
-        const joke = await fetchJoke(jokeId);
-        console.log(joke);
+        const jokeId: string = await fetchJokeId(email) as string;
+        const joke: JokeDTO = await fetchJoke(jokeId) as JokeDTO;
         showJoke(joke);
     } catch (error) {
         console.error("Error:", error)
