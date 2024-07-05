205bed499ce9102bfaf0029babba36d8f39629b7
Azamat Bayramov <bayramov.azamat04@gmail.com>
2024-07-06 02:20:43 +0300

    Add  joke page

--- src/app/joke/page.tsx
+++ src/app/joke/page.tsx
@@ -1,61 +1,24 @@
-'use client';
-
 import React from 'react';
-import { useEffect } from 'react';
-import styled from 'styled-components';
-import { formatDistance } from "date-fns";
-import { JokeDTO, getJokeByEmail } from '../../api/JokeAPI';
+import { getJokeByEmail } from '../../api/JokeAPI';
+import { formatDistance } from 'date-fns';
+import styles from './page.module.css';
 
-const JokePageContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`
 
-const BackButton = styled.a`
-  text-decoration: none;
-
-  background-color: var(--color1);
-  color: var(--color4);
-
-  padding: 0.8em 1em;
-  border-radius: 5px;
-
-  margin-top: 2em;
-
-  transition: background-color 0.3s;
-
-  &:hover {
-    background-color: var(--color3);
-  }
-`
-
-const JokePage: React.FC = () => {
-    const [jokeTitle, setJokeTitle] = React.useState<string>("Joke is loading...");
-    const [jokeDate, setJokeDate] = React.useState<string>("");
-    const [jokeImageSrc, setJokeImageSrc] = React.useState<string>("#");
-    const [jokeImageAlt, setJokeImageAlt] = React.useState<string>("");
-
-    useEffect(() => {
-        getJokeByEmail("a.bayramov@innopolis.university").then((joke: JokeDTO) => {
-            setJokeTitle(joke.safe_title);
-
-            const date = new Date(parseInt(joke.year), parseInt(joke.month) - 1, parseInt(joke.day))
-            setJokeDate(formatDistance(date, new Date(), { addSuffix: true }));
-            
-            setJokeImageSrc(joke.img);
-            setJokeImageAlt(joke.transcript);
-        });
-      }, []);
+export default async function Page() {
+    const joke = await getJokeByEmail("a.bayramov@innopolis.university");
 
     return (
-      <JokePageContainer>
-        <h1 id="jokeTitle">{jokeTitle}</h1>
-        <p id="jokeDate">{jokeDate}</p>
-        <img id="jokeImage" src={jokeImageSrc} alt={jokeImageAlt} />
-        <BackButton href="/">Back to the main page</BackButton>
-      </JokePageContainer>
+        <div className={styles.joke_container}>
+            <h1 id="jokeTitle">{joke.safe_title}</h1>
+            <p id="jokeDate">{
+                formatDistance(
+                    new Date(parseInt(joke.year), parseInt(joke.month) - 1, parseInt(joke.day)),
+                    new Date(),
+                    { addSuffix: true }
+                )
+            }</p>
+            <img id="jokeImage" src={joke.img} alt={joke.transcript} />
+            <a href="/" className={styles.back_button}>Back to the main page</a>
+        </div>
     );
-};
-
-export default JokePage;
+}
