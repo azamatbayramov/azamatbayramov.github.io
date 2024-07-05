2b28275ac70e6784545bc7f619cd98b4c0818381
Azamat Bayramov <bayramov.azamat04@gmail.com>
2024-07-05 21:13:48 +0300

    Fix styles

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Section } from '../Common/Section';
+import styles from '../../styles/section.css'
 
 const AboutMe: React.FC = () => {
   return (
-    <Section id="about-me">
+    <section id="about-me" className={styles.section_container}>
         <h2>About Me</h2>
         <p>
             Hello! I am a Azamat Bayramov. I am from Russia, Ufa. Currently, I am studying at Innopolis University and working as a Python Backend Developer at an outsourcing company. Also, in my free time, I like to photograph, play the guitar and learn new technologies. I try to follow healthy lifestyle, so I go to the swimming pool.
@@ -15,7 +15,7 @@ const AboutMe: React.FC = () => {
             <li>I take my work responsibly and strive for high results, which makes me a valuable and useful member of the team.</li>
             <li>I have a positive attitude and I know how to manage resources competently, which allows me to effectively solve problems and successfully cope with challenges.</li>
         </ul>
-    </Section>
+    </section>
   );
 };
 
