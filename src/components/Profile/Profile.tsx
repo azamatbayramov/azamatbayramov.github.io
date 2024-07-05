10d3e7761f0d1993b05b96fbca060a1c4fa6aa7c
Azamat Bayramov <bayramov.azamat04@gmail.com>
2024-07-05 21:28:19 +0300

    Fix styles

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,41 +1,18 @@
 import React from 'react';
-import styled from 'styled-components';
-
-
-const ProfileContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 1em;
-`
-
-const ProfileImage = styled.img`
-  width: 15em;
-  border-radius: 50%;
-  margin: 0;
-`
-
-const ProfileTitle = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.3em;
-`
-
-const ProfileTitleText = styled.h1`
-  margin: 0;
-`
-
+import styles from './Profile.module.css';
 
 
 const Profile: React.FC = () => {
   return (
-    <ProfileContainer>
-      <ProfileImage src="img/my_photo.jpg" alt="My Photo" />
-      <ProfileTitle>
-        <ProfileTitleText>Azamat Bayramov</ProfileTitleText>
+    <div className={styles.profile_container}>
+      <img src="img/my_photo.jpg" alt="My Photo" className={styles.profile_image} />
+      <div className={styles.profile_title}>
+        <h1 className={styles.profile_title_text}>Azamat Bayramov</h1>
         <p>Python Backend Developer</p>
-      </ProfileTitle>
-    </ProfileContainer>
+      </div>
+    </div>
   );
 };
+
 
 export default Profile;
