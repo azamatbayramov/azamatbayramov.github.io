c9953f435e8d3cf83db7e19615a8a7b8d44e8bd4
Azamat Bayramov <bayramov.azamat04@gmail.com>
2024-07-06 00:37:42 +0300

    Improve main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,10 @@
 import React from 'react';
-import Profile from '../components/Profile/Profile';
-import AboutMe from '../components/AboutMe/AboutMe';
-import Skills from '../components/Skills/Skills';
-import Education from '../components/Education/Education';
-import Contacts from '../components/Contacts/Contacts';
+import MainPage from '../components/MainPage/MainPage';
 
-
-/*
-const MainPageContainer = styled.div`
-  margin: 0;
-  padding: 0;
-`
-
-const MainPageMain = styled.main`
-  background-color: var(--color4);
-
-  display: flex;
-  flex-direction: column;
-
-  gap: 2.5em;
-
-  padding: 2em 10%;
-`
-*/
 
 const Page = () => {
     return (
-      <div>
-        <main>
-          <Profile />
-          <AboutMe />
-          <Skills />
-          <Education />
-          <Contacts />
-        </main>
-      </div>
+        <MainPage/>
     );
 };
 
