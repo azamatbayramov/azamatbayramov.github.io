c0ee89d92156839837ed0e4bc35f4a5b73c17aa2
Azamat Bayramov <bayramov.azamat04@gmail.com>
2024-07-06 00:35:42 +0300

    Improve nav links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,11 +7,11 @@ const Header: React.FC = () => {
     <header className={styles.header_container}>
       <h1>Azamat Bayramov</h1>
       <nav className={styles.header_nav}>
-        <a href="#about-me" className={styles.header_link}>About Me</a>
-        <a href="#skills" className={styles.header_link}>Skills</a>
-        <a href="#education" className={styles.header_link}>Education</a>
-        <a href="#contacts" className={styles.header_link}>Contacts</a>
-        <a href="./joke" className={styles.header_link}>Joke</a>
+        <a href="/#about-me" className={styles.header_link}>About Me</a>
+        <a href="/#skills" className={styles.header_link}>Skills</a>
+        <a href="/#education" className={styles.header_link}>Education</a>
+        <a href="/#contacts" className={styles.header_link}>Contacts</a>
+        <a href="/joke" className={styles.header_link}>Joke</a>
       </nav>
     </header>
   );
