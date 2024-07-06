2f4004a2069a5da5d9f05bfb8b96199d13c07439
Azamat Bayramov <bayramov.azamat04@gmail.com>
2024-07-06 15:23:53 +0300

    Delete unnecessary file

--- rollup.config.js
+++
@@ -1,9 +0,0 @@
-import typescript from '@rollup/plugin-typescript';
-import terser from '@rollup/plugin-terser';
-import resolve from '@rollup/plugin-node-resolve';
-
-export default {
-    input: 'src/comic.ts',
-    output: {file: 'dist/bundle.js'},
-    plugins: [resolve(), typescript(), terser()],
-};
